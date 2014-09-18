52e4070df49b302803f014b3bf964c474df181fc
Halit Olali <halitolali@gmail.com>
2014-09-18 21:10:21 +0300

    wordcount in flambo

--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
-(defproject flambo-plygrnd "0.1.0-SNAPSHOT"
+(defproject
+  flambo-plygrnd "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [yieldbot/flambo "0.4.0-SNAPSHOT"]
+                 [org.apache.spark/spark-core_2.10 "1.1.0"]]
+
+  ;had to be done
+  :aot [flambo-plygrnd.core]
+  )
--- src/flambo_plygrnd/core.clj
+++ src/flambo_plygrnd/core.clj
@@ -1,6 +1,37 @@
-(ns flambo-plygrnd.core)
+(ns flambo-plygrnd.core
+  (:require [flambo.conf :as conf]
+            [flambo.api :as f]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def c (-> (conf/spark-conf)
+           (conf/master "local")
+           (conf/app-name "flame_princess")))
+
+(def sc (f/spark-context c))
+
+(def data (f/parallelize sc [["a" 1] ["b" 2] ["c" 3] ["d" 4] ["e" 5]]))
+
+
+(def data (f/text-file sc "README.md"))
+
+
+;; NOTE: we are using the flambo.api/fn not clojure.core/fn
+(-> data
+    (f/map (f/fn [s] (count s)))
+    (f/reduce (f/fn [x y] (+ x y))))
+
+
+(def data2 (f/text-file sc "LICENSE"))
+
+(defn wc-mapper
+  [line]
+  (let [words (re-seq #"\w+" line)]
+    (partition 2 (interleave words (take (count words) (repeatedly (fn [] 1)))))))
+
+;(defn wc-red [k v acc] (if (nil? acc) v (+ acc v)))
+
+(-> data2
+    (f/flat-map (f/fn [s] (wc-mapper s)))
+    (f/reduce-by-key (f/fn [x y] (do (println x y) (+ x y))))
+    (f/collect))
+
+
