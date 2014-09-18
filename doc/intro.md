956d243a36be27185740e2804f5e0caa1c0486bf
Halit Olali <halitolali@gmail.com>
2014-09-18 20:28:26 +0300

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to flambo-plygrnd
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/flambo_plygrnd/core_test.clj
@@ -0,0 +1,7 @@
+(ns flambo-plygrnd.core-test
+  (:require [clojure.test :refer :all]
+            [flambo-plygrnd.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
